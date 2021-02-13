8e888c2627799f76573761097fad14296d297001
serdarpolat <serdar.plt21@gmail.com>
2021-02-13 21:48:29 +0300

    scr

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:e_commerce/sources/index.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:e_commerce/sources/index.dart';
 
 void main() {
   runApp(MyApp());
